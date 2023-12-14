c3e0c603478639034b5ab0db4a0cbeb9d7f3ccd7
parksuoh <sooho0163@naver.com>
2023-12-14 17:08:51 +0900

    category request change

--- src/main/java/com/camping/camping/controllers/admin/AdminCategoryController.java
+++ src/main/java/com/camping/camping/controllers/admin/AdminCategoryController.java
@@ -3,13 +3,16 @@ package com.camping.camping.controllers.admin;
 import com.camping.camping.applications.AddCategoryService;
 import com.camping.camping.applications.DeleteCategoryService;
 import com.camping.camping.applications.GetAdminCategoriesService;
+import com.camping.camping.dtos.AddCategoryRequestDto;
 import com.camping.camping.dtos.CategoryItemDto;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -40,8 +43,8 @@ public class AdminCategoryController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String post(@RequestPart(value = "categoryName") String categoryName) {
-        return addCategoryService.addCategory(categoryName);
+    public String post(@Valid @RequestBody AddCategoryRequestDto addCategoryRequestDto) {
+        return addCategoryService.addCategory(addCategoryRequestDto.categoryName());
     }
 
     @DeleteMapping("/{categoryId}")
