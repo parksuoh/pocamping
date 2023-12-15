f8b174bd7754010f879e1eb20006ca1617424502
parksuoh <sooho0163@naver.com>
2023-12-15 17:06:27 +0900

    product firstoption change

--- src/main/java/com/camping/camping/applications/AddAdminProductFirstOptionService.java
+++ src/main/java/com/camping/camping/applications/AddAdminProductFirstOptionService.java
@@ -3,12 +3,14 @@ package com.camping.camping.applications;
 
 import com.camping.camping.domains.Product;
 import com.camping.camping.domains.ProductFirstOption;
+import com.camping.camping.domains.ProductSecondOption;
 import com.camping.camping.domains.vo.FirstOptionName;
 import com.camping.camping.domains.vo.Money;
-import com.camping.camping.domains.vo.Name;
+import com.camping.camping.domains.vo.SecondOptionName;
 import com.camping.camping.exceptions.ProductNotExist;
 import com.camping.camping.repositories.ProductFirstOptionRepository;
 import com.camping.camping.repositories.ProductRepository;
+import com.camping.camping.repositories.ProductSecondOptionRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,10 +20,12 @@ public class AddAdminProductFirstOptionService {
 
     private final ProductRepository productRepository;
     private final ProductFirstOptionRepository productFirstOptionRepository;
+    private final ProductSecondOptionRepository productSecondOptionRepository;
 
-    public AddAdminProductFirstOptionService(ProductRepository productRepository, ProductFirstOptionRepository productFirstOptionRepository) {
+    public AddAdminProductFirstOptionService(ProductRepository productRepository, ProductFirstOptionRepository productFirstOptionRepository, ProductSecondOptionRepository productSecondOptionRepository) {
         this.productRepository = productRepository;
         this.productFirstOptionRepository = productFirstOptionRepository;
+        this.productSecondOptionRepository = productSecondOptionRepository;
     }
 
     public String addAdminProductFirstOption(Long productId, String name, Long price) {
@@ -33,6 +37,9 @@ public class AddAdminProductFirstOptionService {
         ProductFirstOption productFirstOption = new ProductFirstOption(product, new FirstOptionName(name), new Money(price));
 
         productFirstOptionRepository.save(productFirstOption);
+
+        ProductSecondOption productSecondOption = new ProductSecondOption(productFirstOption, new SecondOptionName("기본"), new Money(0L));
+        productSecondOptionRepository.save(productSecondOption);
 
         return "success";
     }
